ee027577aa935fab118c738a8f6c229f5f902583
fishimwe1-byte <f.ishimwe1@alustudent.com>
2026-03-20 05:01:36 -0700

    Fix 3-rectangle.py: remove custom __repr__, use default object representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -54,7 +54,3 @@ class Rectangle:
             return ""
         row = "#" * self.__width
         return "\n".join([row for _ in range(self.__height)])
-
-    def __repr__(self):
-        """Return official string representation of the rectangle."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
