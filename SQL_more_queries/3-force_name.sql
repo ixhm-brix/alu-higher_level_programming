162aafed643319045e77dd794e365fda19748c67
fishimwe1-byte <f.ishimwe1@alustudent.com>
2026-03-26 03:03:22 -0700

    Add SQL_more_queries tasks 0-16

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Creates table force_name with a NOT NULL name column
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates database hbtn_0d_usa and table states with a primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
