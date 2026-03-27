5b70db73359f673e46d6df846c91f1aa569ac5c3
fishimwe1-byte <f.ishimwe1@alustudent.com>
2026-03-27 00:45:59 -0700

    Fix 4-hbtn_status.py: use intranet.hbtn.io URL for checker compatibility

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using requests package."""
+"""Fetches https://intranet.hbtn.io/status using requests package."""
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://alu-intranet.hbtn.io/status')
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
