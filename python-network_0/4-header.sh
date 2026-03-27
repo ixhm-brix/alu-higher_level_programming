3583581dd09c87003f5ed93e824a8a8f393ec0d2
fishimwe1-byte <f.ishimwe1@alustudent.com>
2026-03-27 00:09:16 -0700

    Fix 4-header.sh: add explicit http:// scheme for header delivery

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header X-HolbertonSchool-User-Id: 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with header X-HolbertonSchool-User-Id set to 98
+curl -s -H "X-HolbertonSchool-User-Id: 98" "http://$1"
